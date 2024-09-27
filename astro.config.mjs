a3ba880c6a04b37f23d6da443f883aaa4278a220
git_perso_account <ruairidh.cavaye@gmail.com>
2024-09-27 15:57:04 +0100

    Separate basePath for prod and local

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
+const isProd = import.meta.env.PROD;
 // https://astro.build/config
 export default defineConfig({
   site: "https://ruairidhca.github.io",
-  base: "/astro",
+  base: isProd ? "/astro" : "",
 });
